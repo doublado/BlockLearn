6a7e0303a727b0b2bd2f1d48cab4c1b2ca758eaa
doublado <nikolajjak@pm.me>
2025-02-11 19:28:09 +0100

    add Naive UI and Pinia for state management and persistence

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    'nuxtjs-naive-ui',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt'
   ],
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }]
