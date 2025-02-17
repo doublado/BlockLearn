e832e12f19bc0d40d24750b305750bae79db2b24
doublado <nikolajjak@pm.me>
2025-02-17 12:26:27 +0100

    add NDialogProvider to app.vue for enhanced dialog support

--- app.vue
+++ app.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-  import { NMessageProvider } from 'naive-ui';
+  import { NMessageProvider, NDialogProvider } from 'naive-ui';
 </script>
 
 <template>
   <n-message-provider>
+  <n-dialog-provider>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+  </n-dialog-provider>
   </n-message-provider>
 </template>
